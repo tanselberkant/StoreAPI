1be2161f2c424384aefadd5dc8cc4384b3908241
tanselberkant <tanselberkant@gmail.com>
2021-12-11 15:32:18 +0300

    filtering products by their company and name using regex operator

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,17 +1,24 @@
 const Product = require('../models/Product');
 
 const getAllProductsStatic = async (req, res) => {
-  // throw new Error('testing async errors');
-
-  const products = await Product.find({});
+  const search = 'a';
+  const products = await Product.find({
+    name: { $regex: search, $optipns: 'i' },
+  });
   res.status(200).json({ products, numberOfProducts: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured } = req.query;
+  const { featured, company, name } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
   }
   console.log(queryObject);
   const products = await Product.find(queryObject);
