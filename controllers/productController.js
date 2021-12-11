b7e4738a5677fd605e9e9817f43f37e45e53fca0
tanselberkant <tanselberkant@gmail.com>
2021-12-11 15:51:06 +0300

    sorting products by their name and prices

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,15 +1,12 @@
 const Product = require('../models/Product');
 
 const getAllProductsStatic = async (req, res) => {
-  const search = 'a';
-  const products = await Product.find({
-    name: { $regex: search, $optipns: 'i' },
-  });
+  const products = await Product.find({}).sort('-name price');
   res.status(200).json({ products, numberOfProducts: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
@@ -20,8 +17,17 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: 'i' };
   }
+
   console.log(queryObject);
-  const products = await Product.find(queryObject);
+  const result = Product.find(queryObject);
+
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt')
+  }
+  const products = await result;
   res.status(200).json({ products, numberOfProducts: products.length });
 };
 
