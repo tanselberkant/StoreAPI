f2a50500a2ecea132856d718102f4b2fb7d4c1df
tanselberkant <tanselberkant@gmail.com>
2021-12-11 15:17:53 +0300

    Search products by their featured property

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,10 +1,21 @@
+const Product = require('../models/Product');
+
 const getAllProductsStatic = async (req, res) => {
-  throw new Error('testing async errors');
-  res.status(200).json({ msg: 'products testing route' });
+  // throw new Error('testing async errors');
+
+  const products = await Product.find({});
+  res.status(200).json({ products, numberOfProducts: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'products route' });
+  const { featured } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, numberOfProducts: products.length });
 };
 
 module.exports = {
--- populate.js
+++ populate.js
@@ -6,11 +6,13 @@ const jsonProducts = require('./products.json');
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    // await Product.deleteMany(); 
-    await Product.create(jsonProducts)
+    await Product.deleteMany();
+    await Product.create(jsonProducts);
     console.log('Success');
+    process.exit(0);
   } catch (error) {
     console.log(error);
+    process.exit(1);
   }
 };
 start();
